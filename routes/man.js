5fe0f94ee3da6c0a8245e5ebae7b5093929a9f8a
Psychat <keaton.okkonen@gmail.com>
2015-09-11 01:03:57 +1000

    style overhaul

--- routes/man.js
+++ routes/man.js
@@ -17,8 +17,9 @@ function product(product, postage, ref, stage, req, res, locals){
 		root: ref,
 		stage: stage,
 		items: product,
-		addresses: postage,
+		postage: postage,
 		user: req.user,
+		testvar: 'test'
 	}, locals || {} ));
 }
 
@@ -77,7 +78,7 @@ module.exports = function loader(){
 	router.get ('/products/:id', function(req, res){
 		var stage = 1;
 		var ref = 'man/products/' + req.params.id;
-		var postageData;
+		var item;
 		console.log('Loading product: '+ req.params.id);
 		if (req.query.stage) {
 			console.log('Buy stage is :' + req.query.stage);
@@ -87,14 +88,14 @@ module.exports = function loader(){
 			var user = req.user.id;
 		}
 
-		productData = { "title": "Dank-ass leather bag", "price": "$60", "images": [ "http://i00.i.aliimg.com/wsphoto/v3/32224932756_1/Hot-Genuine-Leather-Ladies-Bag-New-Style-Oil-Wax-Women-Handbag-Fashion-2015-Women-Leather-Handbag.jpg", "http://i00.i.aliimg.com/wsphoto/v3/32224932756_2/Hot-Genuine-Leather-Ladies-Bag-New-Style-Oil-Wax-Women-Handbag-Fashion-2015-Women-Leather-Handbag.jpg", "http://i00.i.aliimg.com/wsphoto/v3/32224932756_3/Hot-Genuine-Leather-Ladies-Bag-New-Style-Oil-Wax-Women-Handbag-Fashion-2015-Women-Leather-Handbag.jpg", "http://i00.i.aliimg.com/wsphoto/v3/32224932756_4/Hot-Genuine-Leather-Ladies-Bag-New-Style-Oil-Wax-Women-Handbag-Fashion-2015-Women-Leather-Handbag.jpg", "http://i00.i.aliimg.com/wsphoto/v3/32224932756_5/Hot-Genuine-Leather-Ladies-Bag-New-Style-Oil-Wax-Women-Handbag-Fashion-2015-Women-Leather-Handbag.jpg" ], "sex": "woman", "category": "Accessories", "tags": [ "Handbag", "Interior Slot Pocket", "Zip Pocket", "Cell Phone Pocket", "Soft Leather", "Vintage", "Versatile", "Cow split leather", "Zipper", "700g", "30cm x 40cm x 20cm" ], "option": [ [ [ "Black", "['sku-1-193', 'http://i00.i.aliimg.com/wsphoto/sku/v3/32224932756/32224932756_193/Black-Hot-Genuine-Leather-Ladies-Bag-New-Style-Oil-Wax-Women-Handbag-Fashion-2015-Women-Leather-Handbag.jpg_50x50.jpg']" ], [ "Brown", "['sku-1-365458', 'http://i00.i.aliimg.com/wsphoto/sku/v3/32224932756/32224932756_365458/Brown-Hot-Genuine-Leather-Ladies-Bag-New-Style-Oil-Wax-Women-Handbag-Fashion-2015-Women-Leather-Handbag.jpg_50x50.jpg']" ], [ "Dark Coffee", "['sku-1-200004890', 'http://i00.i.aliimg.com/wsphoto/sku/v3/32224932756/32224932756_200004890/Dark-Grey-Hot-Genuine-Leather-Ladies-Bag-New-Style-Oil-Wax-Women-Handbag-Fashion-2015-Women-Leather-Handbag.jpg_50x50.jpg']" ], [ "Deep Green", "['sku-1-175', 'http://i00.i.aliimg.com/wsphoto/sku/v3/32224932756/32224932756_175/Green-Hot-Genuine-Leather-Ladies-Bag-New-Style-Oil-Wax-Women-Handbag-Fashion-2015-Women-Leather-Handbag.jpg_50x50.jpg']" ], [ "Wine Red", "['sku-1-10', 'http://i00.i.aliimg.com/wsphoto/sku/v3/32224932756/32224932756_10/Red-Hot-Genuine-Leather-Ladies-Bag-New-Style-Oil-Wax-Women-Handbag-Fashion-2015-Women-Leather-Handbag.jpg_50x50.jpg']" ], [ "Dark Blue", "['sku-1-173', 'http://i00.i.aliimg.com/wsphoto/sku/v3/32224932756/32224932756_173/Blue-Hot-Genuine-Leather-Ladies-Bag-New-Style-Oil-Wax-Women-Handbag-Fashion-2015-Women-Leather-Handbag.jpg_50x50.jpg']" ], [ "Rose Red", "['sku-1-200002984', 'http://i00.i.aliimg.com/wsphoto/sku/v3/32224932756/32224932756_200002984/Burgundy-Hot-Genuine-Leather-Ladies-Bag-New-Style-Oil-Wax-Women-Handbag-Fashion-2015-Women-Leather-Handbag.jpg_50x50.jpg']" ] ] ], "rating": "100.0%" };
-		postage.find({ uid: user}, function(err, result) {
+		item = { "title": "Dank-ass leather bag", "price": "$60", "images": [ "http://i00.i.aliimg.com/wsphoto/v3/32224932756_1/Hot-Genuine-Leather-Ladies-Bag-New-Style-Oil-Wax-Women-Handbag-Fashion-2015-Women-Leather-Handbag.jpg", "http://i00.i.aliimg.com/wsphoto/v3/32224932756_2/Hot-Genuine-Leather-Ladies-Bag-New-Style-Oil-Wax-Women-Handbag-Fashion-2015-Women-Leather-Handbag.jpg", "http://i00.i.aliimg.com/wsphoto/v3/32224932756_3/Hot-Genuine-Leather-Ladies-Bag-New-Style-Oil-Wax-Women-Handbag-Fashion-2015-Women-Leather-Handbag.jpg", "http://i00.i.aliimg.com/wsphoto/v3/32224932756_4/Hot-Genuine-Leather-Ladies-Bag-New-Style-Oil-Wax-Women-Handbag-Fashion-2015-Women-Leather-Handbag.jpg", "http://i00.i.aliimg.com/wsphoto/v3/32224932756_5/Hot-Genuine-Leather-Ladies-Bag-New-Style-Oil-Wax-Women-Handbag-Fashion-2015-Women-Leather-Handbag.jpg" ], "sex": "woman", "category": "Accessories", "tags": [ "Handbag", "Interior Slot Pocket", "Zip Pocket", "Cell Phone Pocket", "Soft Leather", "Vintage", "Versatile", "Cow split leather", "Zipper", "700g", "30cm x 40cm x 20cm" ], "option": [ [ [ "Black", "['sku-1-193', 'http://i00.i.aliimg.com/wsphoto/sku/v3/32224932756/32224932756_193/Black-Hot-Genuine-Leather-Ladies-Bag-New-Style-Oil-Wax-Women-Handbag-Fashion-2015-Women-Leather-Handbag.jpg_50x50.jpg']" ], [ "Brown", "['sku-1-365458', 'http://i00.i.aliimg.com/wsphoto/sku/v3/32224932756/32224932756_365458/Brown-Hot-Genuine-Leather-Ladies-Bag-New-Style-Oil-Wax-Women-Handbag-Fashion-2015-Women-Leather-Handbag.jpg_50x50.jpg']" ], [ "Dark Coffee", "['sku-1-200004890', 'http://i00.i.aliimg.com/wsphoto/sku/v3/32224932756/32224932756_200004890/Dark-Grey-Hot-Genuine-Leather-Ladies-Bag-New-Style-Oil-Wax-Women-Handbag-Fashion-2015-Women-Leather-Handbag.jpg_50x50.jpg']" ], [ "Deep Green", "['sku-1-175', 'http://i00.i.aliimg.com/wsphoto/sku/v3/32224932756/32224932756_175/Green-Hot-Genuine-Leather-Ladies-Bag-New-Style-Oil-Wax-Women-Handbag-Fashion-2015-Women-Leather-Handbag.jpg_50x50.jpg']" ], [ "Wine Red", "['sku-1-10', 'http://i00.i.aliimg.com/wsphoto/sku/v3/32224932756/32224932756_10/Red-Hot-Genuine-Leather-Ladies-Bag-New-Style-Oil-Wax-Women-Handbag-Fashion-2015-Women-Leather-Handbag.jpg_50x50.jpg']" ], [ "Dark Blue", "['sku-1-173', 'http://i00.i.aliimg.com/wsphoto/sku/v3/32224932756/32224932756_173/Blue-Hot-Genuine-Leather-Ladies-Bag-New-Style-Oil-Wax-Women-Handbag-Fashion-2015-Women-Leather-Handbag.jpg_50x50.jpg']" ], [ "Rose Red", "['sku-1-200002984', 'http://i00.i.aliimg.com/wsphoto/sku/v3/32224932756/32224932756_200002984/Burgundy-Hot-Genuine-Leather-Ladies-Bag-New-Style-Oil-Wax-Women-Handbag-Fashion-2015-Women-Leather-Handbag.jpg_50x50.jpg']" ] ] ], "rating": "100.0%" };
+
+		postage.find({uid: "facebook|10207595904240496"}, {}, function(err, result) {
 			if (err) throw err;
 			// object of all the users returned in result variable
-			var postageData = result;
+			var postage = result;
+			product(item, postage, ref, stage, req, res);
 		});
-
-		product(productData, postageData, ref, stage, req, res);
 	});
 
 
